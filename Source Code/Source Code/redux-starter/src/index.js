8e9ed36cece7814b94ece657995493837c1c2afe
YONGSU JEONG <53428342+jos50275266@users.noreply.github.com>
2020-07-05 00:30:26 +0900

    Redux_Fundamental + First_Redux_App - Bug Tracker

---
+++ Source Code/Source Code/redux-starter/src/index.js
@@ -0,0 +1,7 @@
+import store from "./store";
+import { bugAdded, bugResolved } from "./actions";
+
+store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
