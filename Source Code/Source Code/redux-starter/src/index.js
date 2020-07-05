fcd9de5ed369769a3cf32acb2b65453362798cd1
YONGSU JEONG <53428342+jos50275266@users.noreply.github.com>
2020-07-05 15:36:02 +0900

    Debugging Redux Applications

--- Source Code/Source Code/redux-starter/src/index.js
+++ Source Code/Source Code/redux-starter/src/index.js
@@ -1,20 +1,20 @@
-// import store from "./store";
-// import { bugAdded, bugRemoved, bugResolved } from "./actions";
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store Changed...", store.getState());
-// });
-
-// store.dispatch(bugAdded("Bug1"));
-// store.dispatch(bugRemoved(1));
-// store.dispatch(bugResolved(1));
+store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugRemoved(1));
+store.dispatch(bugResolved(1));
+store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugAdded("Bug2"));
+store.dispatch(bugAdded("Bug3"));
+store.dispatch(bugRemoved(1));
 
 // Custom Version
-import store from "./customStore";
-import * as actions from "./actions";
+// import store from "./customStore";
+// import * as actions from "./actions";
 
-store.subscribe(() => {
-  console.log("Store Changed!");
-});
-store.dispatch(actions.bugAdded("Bug1"));
-console.log(store.getState());
+// store.subscribe(() => {
+//   console.log("Store Changed!");
+// });
+// store.dispatch(actions.bugAdded("Bug1"));
+// console.log(store.getState());
