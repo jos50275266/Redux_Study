2d589f0262c890ee09272cba27922c262342df89
YONGSU JEONG <jos50275266@gmail.com>
2021-01-21 01:11:12 +0900

    Debugging Redux Applications

--- Redux_Practice/index.js
+++ Redux_Practice/index.js
@@ -1,19 +1,14 @@
 import store from "./store.js";
-console.log(store);
 import { bugAdded, bugResolved, bugRemoved } from "./actions.js";
 
 const unsubscribe = store.subscribe(() => {
     console.log("Store Changed!", store.getState());
 });
 
-store.dispatch(bugResolved(1));
-unsubscribe();
-
-store.dispatch(bugAdded("Bug 1"));
-unsubscribe();
-
-store.dispatch(bugRemoved(1));
-unsubscribe();
+// store.dispatch(bugAdded("Bug 1"));
+// store.dispatch(bugAdded("Bug 2"));
+// store.dispatch(bugAdded("Bug 3"));
+// store.dispatch(bugRemoved(1));
 
 store.dispatch(bugAdded("Bug 2"));
 console.log(store.getState());
